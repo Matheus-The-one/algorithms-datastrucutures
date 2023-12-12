d1ee2c54305245705e4e45c71e1471c40eca9c42
Mate papava <matepapava123@gmail.com>
2023-12-12 19:09:20 +0400

    adding some algos

---
+++ cortesiannumber.js
@@ -0,0 +1,10 @@
+function cortesiannumber(arr1,arr2){
+    let result=[]
+    for(let i=0;i<arr1.length;i++){
+        for(let j=0;arr2.length;i++){
+            result.push(arr1[i],arr2[j])
+        }
+    }
+    return result
+}
+console.log(cortesiannumber([1,4],[3,5,6]))
